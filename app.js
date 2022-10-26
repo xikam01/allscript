e440f3f15f3972c3692c41dcef4220db2803cd6e
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-10-26 01:06:47 -0700

    in this file we added all javascript

---
+++ app.js
@@ -0,0 +1,130 @@
+// // var waa before //Es6
+// //let
+// //consst
+// // = waa synment opearter
+// // waa saddexda Hab ee loo deexo varibales ka javascript
+// // var name = "cabdiraxmaan";
+
+// // let age = 1960;
+// // var year = 2021;
+// // console.log(year - age);
+
+// // console.log("Reading");
+
+// // let magac = "xasan";
+// // let x = 10;
+// // let y = 11;
+
+// // let added = `waa isku gaynta x iyo y  ${x}  ${y}`;
+
+// // console.log(added);
+
+
+// //---->opearter
+// // let x = 50;
+// // let y = 40;
+// // let d = 40;
+// // let f = 50;
+// // let b = 2;
+// // let n = 140;
+// // let employeename = "faarax";
+
+// // let Calculate = (x += y);
+// // let ddone = (Calculate += n);
+// // let ddtwo = (ddone -= f);
+// // let ddthree = (ddtwo /= b);
+// // let ddfoure = ddthree * d;
+// // let amout = `${employeename} mushaharaadkiisu waa $${ddfoure}`;
+// // console.log(amout);
+
+
+// // let mod = 13 % 2;
+// // let moda= mod+=5;
+// // let modb = moda*4;
+// // let modc = modb/=2;
+// // let modd = modc+=30;
+// // let mode =modd-=40;
+// // let modf =mode+23;
+// // let modg =modf*2;
+
+
+// // console.log(modg);
+
+
+// let maanta = "maanta";
+// const iibin = "laiibiyey";
+
+// let litter =2;
+// let price = 8.500;
+// let rec = litter*price;
+// let today= `${maanta} waxaa ${iibin} ${litter}litter oogaas ah oo qiimahoodu yahay ${rec} `
+// console.log(today);
+
+
+//Day two
+//arthematic operators
+// let x = 30;
+// let y = 40;
+// let result = x+=y;
+// console.log(result);
+
+//  let x=3;
+//  let p=4;
+//  let d=50;
+//  let r= 10;
+//  let u=2;
+//  let ii= 4*4-6+100/10*40;
+//  let empol= 'cabdiraxman';
+//  const rec = p-=x;
+//  const red1=rec+d;
+//  let red2=red1-r;
+//  let red3= red2*u;
+//  let red4=red3+ii;
+// const result=`musha haraadka ${empol} uu qaataa waa $${red4}`
+//  console.log(result);
+
+
+//Equal
+//!= not Equal
+// === strict Equal
+// >Greate than
+// >= greate or Equal
+// < less then
+// less than or Equal
+
+// let var1 = 4;
+// let var2 = 10;
+// let var3 = 100;
+// let var4 = 44;
+// let var5 = 2;
+// let var6 = 15;
+
+// console.log( var1 ==4);
+// console.log( var2 !==4);
+// console.log( var3 !=4);
+// console.log( var4 == 4*10+4);
+// console.log( var5 ==4-2);
+// console.log( var6 ==4*4-1);
+
+
+// let var1 = 10;
+// let var2 = 40;
+// let var3 = 140;
+// let var4 = 64;
+// let var5 = 20;
+// let var6 = 55;
+
+// console.log(9 >var1);
+// console.log(20 >var2);
+// console.log(40 > var3);
+// console.log(95< var4);
+// console.log(900<var5);
+// console.log(200 < var6);
+
+// console.log(12 >= var1);
+// console.log(20 <=var2);
+// console.log(130<= var3);
+// console.log(444 !== var4);
+// console.log(490 >= var5);
+// console.log(20 < var6);
+
